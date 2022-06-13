f7b31d0b9af9d2c9f5650a84190a05fc94e83e94
janhavi <janashinde394@gmail.com>
2022-06-13 21:29:12 +1000

    all tests passing (using ubuntu not terminal)

---
+++ db/migrate/001_students.rb
@@ -0,0 +1,8 @@
+class Students < ActiveRecord::Migration
+    create_table :students do |t|
+        t.string :first_name
+        t.string :last_name
+  
+        t.timestamps null: false
+    end
+end
